d478b54559694125b8b47dfb69738ed3d451c9cf
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2021-11-16 15:16:04 +1300

    fix bug

--- R/app_ui.R
+++ R/app_ui.R
@@ -60,7 +60,6 @@ app_ui <- function(request) {
         next_label = shiny::icon("arrow-right", class = "fa-3x"),
         previous_label = shiny::icon("arrow-left", class = "fa-3x"),
         keyboard = FALSE,
-        swipe = FALSE,
         custom_controls = htmltools::tags$div(
           class = "glideControlContainer",
           htmltools::tags$div(
