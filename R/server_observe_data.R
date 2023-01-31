58dee080c2550da65468aa52676b064a23efd63a
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2023-01-31 15:24:14 +1300

    try #32 again

--- R/server_observe_data.R
+++ R/server_observe_data.R
@@ -21,8 +21,7 @@ server_observe_data <- quote({
         values[["site_data"]][, c("id", "description"), drop = FALSE],
         useTypes = TRUE,
         width = whattemplatemaker::get_golem_config("table_width"),
-        stretchH = "all",
-        overflow = "hidden"
+        stretchH = "all"
       ) %>%
       add_hot_col_invalid_id_renderer(
         col = "id",
